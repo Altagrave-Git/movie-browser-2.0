9e265a7834c878c084a8e9f681ca0c6ffc92e502
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-04-30 01:00:08 -0600

    Create initial header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./header.scss";
 import logo from "../../assets/film-icon.svg";
+import { Link, useLocation } from "react-router-dom";
+
+const headerNav = [
+  {
+    label: "Home",
+    path: "/"
+  },
+  {
+    label: "Movies",
+    path: "/movie"
+  },
+  {
+    label: "TV Shows",
+    path: "/tv"
+  }
+];
 
 const Header = () => {
+  const { location } = useLocation();
+  const headerRef = React.useRef(null);
+
+  const active = headerNav.findIndex((item) => item.path === location);
+
   return (
     <header>
       <h1>Header</h1>
