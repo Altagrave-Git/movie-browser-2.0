7d7dfe69f011f727f14d516e4c6f23b9f0df1040
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-04-30 00:59:45 -0600

    Create initial header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.scss";
+import logo from "../../assets/film-icon.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+export default Header;
